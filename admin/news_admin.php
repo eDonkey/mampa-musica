f54e037949f361adf61d75b2c7f87e05176e7e56
Axel <kooltheoutsider@gmail.com>
2017-11-20 15:48:32 -0800

    sd

--- admin/news_admin.php
+++ admin/news_admin.php
@@ -11,14 +11,6 @@
   //]]>
   </script>
   <h4>
-    Default (No Config Specified)
-  </h4>
-  <p>
-    new nicEditor().panelInstance('area1');
-  </p>
-  <textarea cols="50" id="area1">
-</textarea>
-  <h4>
     All Available Buttons {fullPanel : true}
   </h4>
   <p>
@@ -26,31 +18,5 @@
   </p>
   <textarea cols="60" id="area2">
 Some Initial Content was in this textarea
-</textarea>
-  <h4>
-    Change Path to Icon File {iconsPath : 'path/to/nicEditorIcons.gif'}
-  </h4>
-  <p>
-    new nicEditor({iconsPath : 'nicEditorIcons.gif'}).panelInstance('area3');
-  </p>
-  <textarea cols="50" id="area3">
-</textarea>
-  <h4>
-    Customize the Panel Buttons/Select List
-  </h4>
-  <p>
-    {buttonList : ['fontSize','bold','italic','underline','strikeThrough','subscript','superscript']}
-  </p>
-  <textarea cols="50" id="area4">
-HTML content default in textarea
-</textarea>
-  <h4>
-    Set a maximum expansion size (maxHeight)
-  </h4>
-  <p>
-    {maxHeight : 100}
-  </p>
-  <textarea style="height: 100px;" cols="50" id="area5">
-HTML content default in textarea
 </textarea>
 </div>
