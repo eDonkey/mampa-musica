8f964acc8957231aee2fd973a11126f1a7046aa0
Axel <kooltheoutsider@gmail.com>
2017-11-20 15:35:33 -0800

    making tinymce local

--- admin/news_admin.php
+++ admin/news_admin.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+  <script src="../js/tinymce/tinymce.min.js"></script>
   <script>tinymce.init({ selector:'textarea' });</script>
 </head>
 <body>
