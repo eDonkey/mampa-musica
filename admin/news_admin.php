2df6649bc750deabdba922f9ba5412aacf526d56
Axel <kooltheoutsider@gmail.com>
2017-11-20 16:00:38 -0800

    admin

--- admin/news_admin.php
+++ admin/news_admin.php
@@ -1,22 +1,21 @@
-<div id="sample">
-  <script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script> <script type="text/javascript">
-//<![CDATA[
-  bkLib.onDomLoaded(function() {
-        new nicEditor().panelInstance('area1');
-        new nicEditor({fullPanel : true}).panelInstance('area2');
-        new nicEditor({iconsPath : '../nicEditorIcons.gif'}).panelInstance('area3');
-        new nicEditor({buttonList : ['fontSize','bold','italic','underline','strikeThrough','subscript','superscript','html','image']}).panelInstance('area4');
-        new nicEditor({maxHeight : 100}).panelInstance('area5');
-  });
-  //]]>
-  </script>
-  <h4>
-    All Available Buttons {fullPanel : true}
-  </h4>
-  <p>
-    new nicEditor({fullPanel : true}).panelInstance('area2');
-  </p>
-  <textarea cols="60" id="area2">
-Some Initial Content was in this textarea
-</textarea>
-</div>
+<html>
+  <head>
+    <title>Demo 1 : Convert All Textareas</title>
+    <script type="text/javascript" src="../js/nicedit/nicEdit.js"></script>
+    <script type="text/javascript">
+    bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
+    </script>
+  </head>
+  <body>
+    <div id="menu"></div>
+    <br />
+    <div id="sample">
+
+
+      <h4>Third Textarea</h4>
+      <textarea name="area3" style="width: 300px; height: 100px;">
+	HTML <b>content</b> <i>default</i> in textarea
+      </textarea>
+    </div>
+  </body>
+</html>
