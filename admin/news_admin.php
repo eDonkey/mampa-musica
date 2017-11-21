de1094afb55ac6c725c60750b71c5857d57ba084
Axel <kooltheoutsider@gmail.com>
2017-11-20 17:10:41 -0800

    changing write privileges

--- admin/news_admin.php
+++ admin/news_admin.php
@@ -2,7 +2,7 @@
 if (isset($_POST)){
    if (!empty($_POST['area3'])) {
         //Save File
-        $file = fopen("news.html","r+w");
+        $file = fopen("news.html","w");
         $text = $_POST["area3"];
         if (file_put_contents($file, $text)) {
           echo "OK";
