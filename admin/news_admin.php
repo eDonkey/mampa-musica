51ebe8834d2fafe6cb4d439defe0be47b1549bc8
Axel <kooltheoutsider@gmail.com>
2017-11-20 16:12:04 -0800

    fixing plugins sources

--- admin/news_admin.php
+++ admin/news_admin.php
@@ -5,6 +5,24 @@
     <script type="text/javascript">
     bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
     </script>
+    <script src="../js/nicEdit/nicCore/bkLib.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicConfig.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicCore.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicInstance.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicIFrameInstance.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicPanel.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicButton.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCore/nicPlugin.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicPane/nicPane.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicSelect/nicSelect.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicButtonTips/nicButtonTips.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicAdvancedButton/nicAdvancedButton.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicLink/nicLink.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicImage/nicImage.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicCode/nicCode.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicColors/nicColors.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicXHTML/nicXHTML.js" type="text/javascript"></script>
+    <script src="../js/nicEdit/nicBBCode/nicBBCode.js" type="text/javascript"></script>
   </head>
   <body>
     <div id="menu"></div>
