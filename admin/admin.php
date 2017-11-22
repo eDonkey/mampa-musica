4b5a712fb2ea47270ed44f848d6aec7fdfb2c04e
Axel <kooltheoutsider@gmail.com>
2017-11-21 16:31:10 -0800

    configuring admin

--- admin/admin.php
+++ admin/admin.php
@@ -4,7 +4,22 @@ if(!isset($_SESSION['username'])) {
   header("location: index.php");
   exit;
 } else {
-  echo "hola";
-  die(var_dump($_SESSION));
-}
+  //echo "hola";
+  //die(var_dump($_SESSION));
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mampa - Musica | Admin Dashboard</title>
+  </head>
+  <body>
+    <div id="admin-top-menu">
+      Inicio - Noticias
+    </div>
+    <br /><br />
+    <div id="admin-body-loader">
+      <iframe id="admin-page-loader" name="admin-page-loader" width="500px" height="500px" border="2"></iframe>
+    </div>
+  </body>
+</html>
+<?php } ?>
