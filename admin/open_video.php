d58be12a67b888c8bea8faa68baebcc28c8eb03b
Axel <kooltheoutsider@gmail.com>
2017-11-28 15:27:03 -0800

    handling open video

---
+++ admin/open_video.php
@@ -0,0 +1,21 @@
+<?php
+include '../lib/mysql/basic.config.mysql.php';
+session_start();
+if(!isset($_SESSION['username'])) {
+  header("location: index.php");
+  exit;
+}
+$id = $_GET['id'];
+$url = $_GET['url'];
+$query = "UPDATE videos SET clicks = clicks + 1 WHERE id=$id LIMIT 1";
+//die(var_dump($query));
+$link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+mysqli_query($link, $query) or die("Error executing update sentense. ".mysqli_error($link));
+header("Location: ".$url);
+?>
--- admin/videos_admin.php
+++ admin/videos_admin.php
@@ -60,7 +60,7 @@ while ($row = mysqli_fetch_array($result)) {
           <tr>
             <td><?php echo $row['id']; ?></td>
             <td><?php echo $row['youtube_key']; ?></td>
-            <td><a href="<?php echo $row['video']; ?>" target="_blank">>>LINK HERE<<</a></td>
+            <td><a href="open_video.php?url=<?php echo $row['video']; ?>&id=<?php echo $row['id']; ?>" target="_blank">>>LINK HERE<<</a></td>
             <td><img src="<?php echo $row['thumb']; ?>" width="84" height="47" /></td>
             <td><?php echo $row['clicks']; ?></td>
             <td><a href="videos_admin_delete.php?id=<?php echo $row['id']; ?>"><img src="imgs/delete.png" border="0" width="24" height="24" /></a></td>
