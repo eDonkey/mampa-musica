36a764ec371daafb712bfbadd0955ad20642d784
Axel <kooltheoutsider@gmail.com>
2017-11-28 11:37:30 -0800

    videos admin

--- admin/videos_admin.php
+++ admin/videos_admin.php
@@ -1,10 +1,12 @@
 <?php
+include '../lib/mysql/basic.config.mysql.php';
 $subject = $_GET['url'];
 $search = 'https://www.youtube.com/watch?v=' ;
 $trimmed = str_replace($search, '', $subject) ;
 $thumb = "https://i.ytimg.com/vi/".$trimmed."/hqdefault.jpg";
-echo "URL del video: ".$subject;
-echo "<br />";
-echo "URL del thumb: ".$thumb;
-
+//echo "URL del video: ".$subject;
+//echo "<br />";
+//wecho "URL del thumb: ".$thumb;
+$query = "INSERT INTO videos SET youtube_key='$trimmed', video='$subject', thumb='$thumb'";
+die(var_dump($query));
 ?>
