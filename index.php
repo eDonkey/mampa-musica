0650a4c24dbc458b59ef010e84e44aa3a674c764
Axel <kooltheoutsider@gmail.com>
2017-11-28 15:51:32 -0800

    sda

--- index.php
+++ index.php
@@ -184,7 +184,7 @@ $result = mysqli_query($link, $videos);
 <?php while ($row = mysqli_fetch_array($result)) { ?>
           <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
             <div class="box">
-              <p class="description"><a href="admin/open_video.php?url=<?php echo $row['video']; ?>&id=<?php echo $row['id']; ?>" target="_blank"><img src="<?php echo $row['thumb']; ?>" border="0" width="350" height="200" /></a></p>
+              <p class="description"><a href="admin/open_video.php?url=<?php echo $row['video']; ?>&id=<?php echo $row['id']; ?>" target="_blank"><img src="<?php echo $row['thumb']; ?>" border="0" width="175" height="100" /></a></p>
             </div>
           </div>
 <?php } ?>
