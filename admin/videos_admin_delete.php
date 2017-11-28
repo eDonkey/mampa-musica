f2fd6ab7b9ae36fe11cfc336abcb04b21a2dc900
Axel <kooltheoutsider@gmail.com>
2017-11-28 14:09:56 -0800

    debugging2

--- admin/videos_admin_delete.php
+++ admin/videos_admin_delete.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION['username'])) {
 }
 $id = $_GET['id'];
 $query = "DELETE FROM videos WHERE id='$id' LIMIT 1";
-die(var_dump($query));
+//die(var_dump($query));
 $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -15,6 +15,6 @@ if (!$link) {
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-mysqli_query($link, $query) or die("Error executing delete sentense");
+mysqli_query($link, $query) or die("Error executing delete sentense".mysqli_error());
 header("Location: videos_admin.php?operation=video_del&result=success");
 ?>
