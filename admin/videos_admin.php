8da59c43cf73491c23184504f9e3dfc8a87741e5
Axel <kooltheoutsider@gmail.com>
2017-11-28 11:21:00 -0800

    sad

--- admin/videos_admin.php
+++ admin/videos_admin.php
@@ -2,8 +2,9 @@
 $subject = $_GET['url'];
 $search = 'https://www.youtube.com/watch?v=' ;
 $trimmed = str_replace($search, '', $subject) ;
+$thumb = "https://i.ytimg.com/vi/".$trimmed."/hqdefault.jpg";
 echo "URL del video: ".$subject;
 echo "<br />";
-echo "URL del thumb: https://i.ytimg.com/vi/".$trimmed."/maxresdefault.jpg";
-echo $trimmed ;
+echo "URL del thumb: ".$thumb;
+
 ?>
