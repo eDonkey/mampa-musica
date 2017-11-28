6ccf3e55b40862f73a23740ade8546db8b3abe20
Axel <kooltheoutsider@gmail.com>
2017-11-28 12:40:35 -0800

    videos admin #2

--- admin/videos_admin.php
+++ admin/videos_admin.php
@@ -3,3 +3,13 @@ if ($_GET['operation'] == "video_add" AND $_GET['result'] == "success") {
   echo "Video added correctly to our DB";
 }
 ?>
+<html>
+  <head>
+    <title>Videos</title>
+  </head>
+  <body>
+    <form method="post" action="videos_admin_add.php">
+      Video URL (Solo youtube): <input type="text" name="video_url" id="video_url" /><input type="submit" value="Cargar" />
+    </form>
+  </body>
+</html>
