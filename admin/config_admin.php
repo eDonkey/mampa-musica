888cdcc8f02ad682496db49f0a76ddab1c1c1db2
Axel <kooltheoutsider@gmail.com>
2018-08-14 16:27:18 -0300

    adding dates background

--- admin/config_admin.php
+++ admin/config_admin.php
@@ -3,6 +3,6 @@
 $mysqlfile = file_get_contents('../lib/mysql/basic.config.mysql.php');
 ?>
 <form method="post" action="">
-<textarea name="mysql_config_text" style="width: 1268px; height: 606px;"><?php echo $mysqlfile; ?></textarea>
+<textarea name="mysql_config_text" style="width: 1267px; height: 587px;"><?php echo $mysqlfile; ?></textarea>
     <input type="submit" value="save" />
 </form>
