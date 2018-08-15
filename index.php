2a7d4ecbcf80ba001279cd964126196948cfd447
Axel <kooltheoutsider@gmail.com>
2018-08-15 17:04:30 -0300

    adding matomo to index

--- index.php
+++ index.php
@@ -53,7 +53,21 @@ mysqli_set_charset($link, "utf8");
 
   <!-- Template Main Javascript File -->
   <script src="js/main.js"></script>
-
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//104.236.175.157/mampa/analytics/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
   <!-- =======================================================
     Theme Name: Regna
     Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
