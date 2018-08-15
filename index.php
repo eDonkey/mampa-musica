2916a44e5a70cd2d26ad0e1ce819352cacd27b4d
Axel <kooltheoutsider@gmail.com>
2018-08-15 12:19:07 -0300

    sda

--- index.php
+++ index.php
@@ -253,7 +253,10 @@ $result = mysqli_query($link, $videos);
       <div class="container wow fadeIn">
         <div class="row">
           <div class="col-lg-9 text-center text-lg-left">
-            <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/216220403&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+            <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/216220403&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            Aca iria un texto indicativo y alusivo al tipo de musica o lo que quisieran poner que acompañe este hermoso rectangulo de soundcloud
           </div>
         </div>
 
