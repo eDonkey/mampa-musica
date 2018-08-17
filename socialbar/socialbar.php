1f672672712ddbb28ef85dab461dce9261f0388b
Axel <kooltheoutsider@gmail.com>
2018-08-17 15:20:59 -0300

    increasing animation degrees

--- socialbar/socialbar.php
+++ socialbar/socialbar.php
@@ -2,7 +2,6 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-        <link href="../css/demo-page.css" rel="stylesheet" media="all">
         <link href="../css/hover.css" rel="stylesheet" media="all">
     </head>
     <body>
