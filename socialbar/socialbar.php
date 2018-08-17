e6b304f20f3c6bafdd0cafd5e32334dae6c7ec50
Axel <kooltheoutsider@gmail.com>
2018-08-17 15:06:32 -0300

    sd

--- socialbar/socialbar.php
+++ socialbar/socialbar.php
@@ -6,13 +6,13 @@
         <link href="../css/hover.css" rel="stylesheet" media="all">
     </head>
     <body>
-        <div>
-            <a href="#" class="button"><img src="fb.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="button"><img src="ig.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="button"><img src="sc.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="button"><img src="sp.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="button"><img src="tw.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="button"><img src="yt.png" border="0" width="32px" height="32px" /></a>
+        <div id="effects" class="effects">
+            <a href="#" class="hvr-grow-rotate"><img src="fb.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="hvr-grow-rotate"><img src="ig.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="hvr-grow-rotate"><img src="sc.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="hvr-grow-rotate"><img src="sp.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="hvr-grow-rotate"><img src="tw.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="hvr-grow-rotate"><img src="yt.png" border="0" width="32px" height="32px" /></a>
         </div>
     </body>
 </html>
