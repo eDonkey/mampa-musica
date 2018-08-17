3f542cffdfea4bfa3d0621d4a7e36ea0c6ee153e
Axel <kooltheoutsider@gmail.com>
2018-08-17 13:50:40 -0300

    sda

--- socialbar/socialbar.php
+++ socialbar/socialbar.php
@@ -1,8 +1,15 @@
-<div>
-    <img src="fb.png" border="0" width="32px" height="32px" />
-    <img src="ig.png" border="0" width="64px" height="64px" />
-    <img src="sc.png" border="0" />
-    <img src="sp.png" border="0" />
-    <img src="tw.png" border="0" />
-    <img src="yt.png" border="0" />
-</div>
+<html>
+    <head>
+        <link href="../css/hover.css" rel="stylesheet" media="all">
+    </head>
+    <body>
+        <div>
+            <a href="#" class="button"><img src="fb.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="button"><img src="ig.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="button"><img src="sc.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="button"><img src="sp.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="button"><img src="tw.png" border="0" width="32px" height="32px" /></a>
+            <a href="#" class="button"><img src="yt.png" border="0" width="32px" height="32px" /></a>
+        </div>
+    </body>
+</html>
