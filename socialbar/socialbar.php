bed44b221cfa67d992f705085ecf5174645cb9f1
Axel <kooltheoutsider@gmail.com>
2018-08-17 13:37:42 -0300

    socialbar created

---
+++ socialbar/socialbar.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="fb.png" border="0" /><img src="ig.png" border="0" /><img src="sc.png" border="0" /><img src="sp.png" border="0" /><img src="tw.png" border="0" /><img src="yt.png" border="0" />
+</div>
