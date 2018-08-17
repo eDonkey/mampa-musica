936be78c0a49679ef5c23db70d9f53687a1fdede
Axel <kooltheoutsider@gmail.com>
2018-08-17 15:44:54 -0300

    fixing path

--- socialbar/socialbar.php
+++ socialbar/socialbar.php
@@ -1,4 +1,3 @@
-<?php include("../lib/mysql/basic.config.mysql.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -7,12 +6,12 @@
     </head>
     <body>
         <div id="effects" class="effects">
-            <a href="<?php echo $fburl; ?>" target="_blank" class="hvr-grow-rotate"><img src="fb.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
-            <a href="<?php echo $igurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="ig.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
-            <a href="<?php echo $scurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="sc.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
-            <a href="<?php echo $spurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="sp.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
-            <a href="<?php echo $twurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="tw.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
-            <a href="<?php echo $yturl; ?>" target="_blank" class="hvr-grow-rotate"><img src="yt.png" border="0" width="32px" height="32px" /></a>
+            <a href="<?php echo $fburl; ?>" target="_blank" class="hvr-grow-rotate"><img src="socialbar/fb.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $igurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="socialbar/ig.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $scurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="socialbar/sc.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $spurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="socialbar/sp.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $twurl; ?>" target="_blank" class="hvr-grow-rotate"><img src="socialbar/tw.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $yturl; ?>" target="_blank" class="hvr-grow-rotate"><img src="socialbar/yt.png" border="0" width="32px" height="32px" /></a>
         </div>
     </body>
 </html>
