112d3c342080b8e1efb85720043b1d5a7de0adf0
Axel <kooltheoutsider@gmail.com>
2018-08-17 15:41:18 -0300

    social urls

--- lib/mysql/basic.config.mysql.php
+++ lib/mysql/basic.config.mysql.php
@@ -4,7 +4,12 @@ $dbpass = "EtEWA85csCxrGrLU";
 $dbhost = "localhost";
 $dbname = "mampa";
 $videodisplaylimit = 4;
-
+$fburl = "https://www.facebook.com/mampa.musica";
+$twurl = "https://twitter.com/MampaOficial";
+$igurl = "https://instagram.com/mampa.musica";
+$scurl = "https://soundcloud.com/mampamusica";
+$yturl = "https://www.youtube.com/channel/UCGpVIjK5wAH-hZT-gCwSPjQ";
+$spurl = "https://www.spotify.com/";
 //777 = superuser
 //775 = admin
 //755 = moderator
--- socialbar/socialbar.php
+++ socialbar/socialbar.php
@@ -1,3 +1,4 @@
+<?php include("../lib/mysql/basic.config.mysql.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -6,12 +7,12 @@
     </head>
     <body>
         <div id="effects" class="effects">
-            <a href="#" class="hvr-grow-rotate"><img src="fb.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="hvr-grow-rotate"><img src="ig.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="hvr-grow-rotate"><img src="sc.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="hvr-grow-rotate"><img src="sp.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="hvr-grow-rotate"><img src="tw.png" border="0" width="32px" height="32px" /></a>
-            <a href="#" class="hvr-grow-rotate"><img src="yt.png" border="0" width="32px" height="32px" /></a>
+            <a href="<?php echo $fburl; ?>" class="hvr-grow-rotate"><img src="fb.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $igurl; ?>" class="hvr-grow-rotate"><img src="ig.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $scurl; ?>" class="hvr-grow-rotate"><img src="sc.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $spurl; ?>" class="hvr-grow-rotate"><img src="sp.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $twurl; ?>" class="hvr-grow-rotate"><img src="tw.png" border="0" width="32px" height="32px" /></a>&nbsp;&nbsp;
+            <a href="<?php echo $yturl; ?>" class="hvr-grow-rotate"><img src="yt.png" border="0" width="32px" height="32px" /></a>
         </div>
     </body>
 </html>
