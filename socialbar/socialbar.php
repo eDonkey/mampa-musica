6720f62e3446e7068d4d599b199f7d4f83d00296
Axel <kooltheoutsider@gmail.com>
2018-08-17 13:39:49 -0300

    trying many icons size

--- socialbar/socialbar.php
+++ socialbar/socialbar.php
@@ -1,3 +1,8 @@
 <div>
-    <img src="fb.png" border="0" /><img src="ig.png" border="0" /><img src="sc.png" border="0" /><img src="sp.png" border="0" /><img src="tw.png" border="0" /><img src="yt.png" border="0" />
+    <img src="fb.png" border="0" width="32px" height="32px" />
+    <img src="ig.png" border="0" width="64px" height="64px" />
+    <img src="sc.png" border="0" />
+    <img src="sp.png" border="0" />
+    <img src="tw.png" border="0" />
+    <img src="yt.png" border="0" />
 </div>
