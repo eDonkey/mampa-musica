765e1f4e3b76f7709497bf542251c83e6a4c54f9
Axel Wdoviak <kooltheoutsider@gmail.com>
2018-09-09 21:59:19 -0300

    fixing path agin

--- admin/fotosmanager/php/connector.minimal.php
+++ admin/fotosmanager/php/connector.minimal.php
@@ -122,8 +122,8 @@ $opts = array(
 		// Items volume
 		array(
 			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
-			'path'          => '../../',                 // path to files (REQUIRED)
-			'URL'           => dirname($_SERVER['PHP_SELF']) . '/../../', // URL to files (REQUIRED)
+			'path'          => '../../../albums/',                 // path to files (REQUIRED)
+			'URL'           => dirname($_SERVER['PHP_SELF']) . '../../../albums/', // URL to files (REQUIRED)
 			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
@@ -135,8 +135,8 @@ $opts = array(
 		array(
 			'id'            => '1',
 			'driver'        => 'Trash',
-			'path'          => '../../albums/.trash/',
-			'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/../../albums/.trash/.tmb/',
+			'path'          => '../../../albums/.trash/',
+			'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '../../../albums/albums/.trash/.tmb/',
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
 			'uploadAllow'   => array('image', 'text/plain'),// Same as above
