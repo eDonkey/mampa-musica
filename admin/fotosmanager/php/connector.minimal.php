d28feb4d17e11b2035ba51df6b571960b49c1d4d
Axel <kooltheoutsider@gmail.com>
2018-09-10 13:30:47 -0300

    disable trashHash

--- admin/fotosmanager/php/connector.minimal.php
+++ admin/fotosmanager/php/connector.minimal.php
@@ -124,7 +124,7 @@ $opts = array(
 			'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
 			'path'          => '../../../albums/',                 // path to files (REQUIRED)
 			'URL'           => dirname($_SERVER['PHP_SELF']) . '../../../albums/', // URL to files (REQUIRED)
-			'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
+			//'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
 			'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
 			'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
 			'uploadAllow'   => array('image', 'text/plain'),// Mimetype `image` and `text/plain` allowed to upload
